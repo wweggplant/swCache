d7bffcd976c2c6b68a85eedd6095e875b17bd9e5
wangjojo <weiainijiujiu@126.com>
2016-10-29 15:56:11 +0800

    init

--- dist/localStorage.js
+++ dist/localStorage.js
@@ -1,83 +1,4 @@
-/*
-    localStorage
-
-
-
-
- */
-
-;(function(window,document){
-    "use strict";
-    //是否支持LocalStorage
-    var manager,//管理器
-        resourceMap = {
-            "script":"src",
-            "url":"href",
-            "img":"src"
-        },
-        storagePrefix = 'localStorage-';
-    function isSupportLocalStorage() {
-        return true;
-    }
-    function $$(selector){
-        return  document.querySelectorAll(selector)&&Array.prototype.slice.apply(document.querySelectorAll(selector));
-    }
-    function getPlainTextByAjax(url){
-        var xhr,queue=[];
-        xhr = new XMLHttpRequest();
-        xhr.open("get",url,true);
-        xhr.send();
-
-        xhr.onreadystatechange = function(){
-            //回调队列
-            if (xhr.readyState==4 && xhr.status ==200&&queue.length>0) {
-                queue.forEach(function(cb){
-                    cb&&cb.call&&cb.call(this,xhr,xhr.reponseText);
-                });
-            }
-        };
-        return {
-            xhr:xhr,
-            done:function(cb){
-                 if (cb) {
-                    queue.push(cb);
-                }
-            }
-        };
-    }
-    //判断类型
-    Resource.prototype.getType = function(makup){
-        return makup&&makup.nodeName&&makup.nodeName.toLowerCase();
-    };
-
-    //资源类
-    function Resource(makup){
-        if(this===window){
-            return new Resource(makup);
-        }
-        this.init(makup);
-    }
-
-    /*
-        返回对象信息
-     */
-    Resource.prototype.getInfo = function(){
-        var  r = this;
-        return {
-            name:r.name,
-            type:r.type,
-            url:r.url,
-            makup:r.makup,
-        };
-    };
-    Resource.prototype.init = function(makup){
-        var nameReg = /\/?([a-z\-\.]*).js/g;
-        this.makup = makup;
-        this.type = this.getType(makup);
-        this.url = makup.getAttribute("data-local");
-        this.name = (nameReg.exec(this.url))[1];
-    };
-    manager = function(version){
+!function(t,e){"use strict";function r(){return!0}function n(t){return e.querySelectorAll(t)&&Array.prototype.slice.apply(e.querySelectorAll(t))}function o(t){var e,r=[];return e=new XMLHttpRequest,e.open("get",t,!0),e.send(),e.onreadystatechange=function(){4==e.readyState&&200==e.status&&r.length>0&&r.forEach(function(t,r){r&&r.call&&r.call(this,e,e.reponseText)})},{xhr:e,done:function(t){t&&r.push(t)}}}function i(e){return this===t?new i(e):void this.init(e)}var u,a={script:"src",url:"href",img:"src"},s="localStorage-";i.prototype.getType=function(t){return t&&t.nodeName&&t.nodeName.toLowerCase()},i.prototype.getInfo=function(){var t=this;return{name:t.name,type:t.type,url:t.url,makup:t.makup}},i.prototype.init=function(t){var e=/\/([a-z\-\.]*).js/g;this.makup=t,this.type=this.getType(t),this.url=t.getAttritube("data-local"),this.name=e.exec(this.url)[1]},u=function(t){var u,c,l={},p=localStorage,f="storageInfo",g="data-local";c={isSupport:function(){return!0},get:function(t){if(c.isSupport())return JSON.parse(p.getItem(t))},remove:function(t){p.removeItem(t)},set:function(t,e){if(c.isSupport())try{p.removeItem(t),p.setItem(t,JSON.stringify(e))}catch(o){if(o.name.toUpperCase().indexOf("QUOTA")>=0){var r,n=[];for(r in p)0===r.indexOf(s)&&n.push(JSON.parse(p[r]));return n.length?(n.sort(function(t,e){return t.stamp-e.stamp}),c.remove(n[0].key),c.set(t,e)):void 0}return}}},u={require:function(t){var e=new i(t),r=o(e.url);return r.always(function(t,r){var n=e[n];c.set(n,r);var o=u.getAll();o[n]=e.getInfo(),c.set(f,o)}),r},getAll:function(){return c.get(f)},getVersion:function(){return c.get(t)},updateVersion:function(t){c.set("version",t)}};var h=n("["+g+"]");if(!r())return h.forEach(function(t,e){var r=e.getAttribute(g);e.setAttribtue(g,r),e.removeAttribute(g)}),!1;if(t===u.getVersion()){l=u.getAll();for(var m in l){var v=e.createNode(m.type);v.setAttribute(a[m.type],m.url),e.appendChild(v)}}else h.forEach(function(t,e){u.require(e)}),u.updateVersion(t);return u},t.localManager||(t.localManager=u)}(this,document);er = function(version){
         var resources = {},
             loader,
             local = localStorage,
@@ -90,12 +11,7 @@
             },
             get:function(name){
                 if(storage.isSupport()){
-                    try{
-                        return JSON.parse(local.getItem(name));
-                    }catch(e){
-                        return local.getItem(name);
-                    }
-
+                    return JSON.parse(local.getItem(name));
                 }
             },
             remove:function(name){
@@ -146,12 +62,12 @@
             require:function(markup){
                 var resource = new Resource(markup);
                 var xhr = getPlainTextByAjax(resource.url);
-                xhr.done(function(xhr,text){
-                    var name = resource.name;
+                xhr.always(function(xhr,text){
+                    var name = resource[name];
                     //保存文本
                     storage.set(name,text);
                     //更新信息表
-                    resources = loader.getAll()||{};
+                    var resources = loader.getAll();
                     resources[name]  = resource.getInfo();
                     storage.set(storageInfoName,resources);
                 });
@@ -161,7 +77,7 @@
                 return  storage.get(storageInfoName);
             },
             getVersion:function(){
-                return storage.get("version");
+                return storage.get(version);
             },
             updateVersion:function(version){
                 storage.set("version",version);
@@ -171,7 +87,7 @@
         //1.是否支持
         if (!isSupportLocalStorage()) {
             //不支持本地储存,使用本来的方式处理
-            markups.forEach(function(markup){
+            markups.forEach(function(i,markup){
                 var url = markup.getAttribute(prefix);
                 markup.setAttribtue(prefix,url);
                 markup.removeAttribute(prefix);
@@ -189,7 +105,7 @@
             }
         }else{
             //请求本地资源并保存在local中
-            markups.forEach(function(markup){
+            markups.forEach(function(i,markup){
                 loader.require(markup);
             });
             //保存版本信息
--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,6 @@
 var dist = "./dist/";
 //src目录是我们的源代码
 var src = "./src/";
-var static = "Public/";
-var paths = {
-    tpl:"Portal/**/*.html",
-    css:"css/**/*.css",
-    images:"images/**.*",
-    js:"js/**/*.js",
-    lib:{
-        boostrap:"simpleboot/**/*.*"
-    }
-};
-
 /*
   引入要使用的插件
 */
@@ -26,30 +15,13 @@ var cleanCSS = require('gulp-clean-css');
 var del = require('del');
 var uglify = require('gulp-uglify');
 var del = require('del');
-var s = src + static,
-    d = dist + static;
-//不用编译的文件复制到生成环境中
-gulp.task("copy",function (cb) {
-    gulp.src(src+"*.html")
-        .pipe(gulp.dest(dist));
-    gulp.src(s+paths.images)
-        .pipe(gulp.dest(d+"images"));
-    gulp.src(s+paths.lib.boostrap)
-        .pipe(gulp.dest(d+"simpleboot/"));
-});
-
-//压缩css
-gulp.task("compressCss",function(){
-    return gulp.src(s+paths.css)
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest(d+"css"));
-});
 //压缩js
 gulp.task('compressJs',function () {
-    return gulp.src(s+paths.js)
+    gulp.src(src+"localStorage.js")
+            .pipe(gulp.dest(dist));
+    gulp.src(src+"localStorage.js")
             .pipe(uglify())
-            .pipe(gulp.dest(d+"js"));
-
+            .pipe(gulp.dest(dist));
 });
 //我们最后要运行的命令就是`gulp default`;
-gulp.task('default', ['copy','compressJs','compressHtml']);
+gulp.task('default', ['compressJs']);
